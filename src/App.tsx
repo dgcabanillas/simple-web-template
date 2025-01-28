e17ded5bfbd0dd8b1ef9ca7945ce98d8817b30b4
Diego Cabanillas <IR48445894@intercorpretail.pe>
2025-01-28 18:09:10 -0500

    feat(app): Simple Web Template

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <div className='container'>
+        <span className='text text--primary'>
+          Este es un texto de color primario
+        </span>
+        <span className='text text--secondary'>
+          Este es un texto de color secundario
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default App
